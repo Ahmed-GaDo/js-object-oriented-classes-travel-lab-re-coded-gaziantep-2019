e01dca18575393629eb88909f0ee0d04551d8baf
Ahmed Gado <a.gado.2df@gmail.com>
2019-08-17 10:28:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 let startDate = 1995;
  let endDate = 2017;
+class Driver {
+    constructor(name, startDate){
+        this.name = name ;
+        this.startDate = new Date(startDate);
+      }
+      yearsExperienceFromBeginningOf(endDate) {
+        this.endDate = new Date(endDate);
+        return this.endDate.getFullYear() - this.startDate.getFullYear();
+      }
+    }
+    
+    class Route {
+      constructor (startLocation, endLocation){
+        this.startLocation = {horizontal : park, vertical : 34};
+        this.endLocation = {horizontal : park, vertical : 45};
+      }
+      blocksTravelled() {
+        return this.endLocation - this.startLocation ;
+      }
+    }
+    
+    
